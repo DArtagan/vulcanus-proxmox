abddd1b5b3bdba6526b550690f2ebe38a0b187b8
William Weiskopf <william@weiskopf.me>
2026-08-31 17:19:14 -0700

    Update.

--- devenv.nix
+++ devenv.nix
@@ -27,13 +27,14 @@
     opentofu.enable = true;
   };
 
+  claude.code.enable = true;
   dotenv.enable = true;
 
   git-hooks.hooks = {
     end-of-file-fixer.enable = true;
     deadnix.enable = true;
     flake-checker.enable = true;
-    nixfmt-rfc-style.enable = true;
+    nixfmt.enable = true;
     shellcheck.enable = true;
     statix.enable = true;
     tflint.enable = true;
